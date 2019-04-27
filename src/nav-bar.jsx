46aaec09e74ecee97214e77c2f0d9e6aceb4775d
Nesu11 <anesujane98@gmail.com>
2019-04-27 09:18:10 +0100

    nav-styling

--- src/nav-bar.jsx
+++ src/nav-bar.jsx
@@ -1,6 +1,24 @@
 import React from 'react';
 import '../styles/navBar.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faIgloo } from '@fortawesome/free-solid-svg-icons';
 
-const NavBar = () => <div> Nav Bar </div>;
+library.add(faIgloo);
+
+const NavBar = () => (
+  <div>
+    <div className="nav-bar"> My Nav Bar </div>
+    <div className="logo">
+      <FontAwesomeIcon icon="igloo" />
+      Surreal Estate
+    </div>
+    <ul className="nav">
+      <li className="item" />
+      <li className="item" />
+    </ul>
+  </div>
+
+);
 
 export default NavBar;
