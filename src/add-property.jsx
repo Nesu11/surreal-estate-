ab15723be4dd0b12fcc065d4b4d0853c97faf8f5
Nesu11 <anesujane98@gmail.com>
2019-05-06 12:23:13 +0100

    bind this

--- src/add-property.jsx
+++ src/add-property.jsx
@@ -2,8 +2,23 @@ import React from 'react';
 import '../styles/addProperty.css';
 
 class AddProperty extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {};
+
+    this.handleAddProperty = this.handleAddProperty.bind(this);
+  }
+  handleAddProperty(event) {
+    event.preventDefault()
+  }
+
   render() {
-    return <div className="AddProperty"> Add Propert Page </div>;
+    return (
+      <div className="AddProperty"> Add Propert Page
+        <form onSubmit={this.handleAddProperty}> forma </form>
+      </div>
+    );
   }
 }
 
