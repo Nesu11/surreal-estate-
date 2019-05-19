e67a8faf8ad2f9238a95d02241f360308d70af61
Nesu11 <anesujane98@gmail.com>
2019-05-19 17:05:32 +0100

    filter

--- src/properties.jsx
+++ src/properties.jsx
@@ -1,20 +1,74 @@
 import React from 'react';
 import PropertyCard from './property-card';
 import '../styles/properties.css';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import qs from 'qs'
+
+const URL_DATA = 'http://localhost:3000/api/v1/PropertyListing';
 
 
-const Properties = () => (
-  <div className="properties">
-    <PropertyCard
-      title="Lovely period property"
-      type="Detached"
-      bedrooms={4}
-      bathrooms={3}
-      price={345678}
-      city="Manchester"
-      email="mock@email.com"
-    />
-  </div>
-);
+class Properties extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      properties: [],
+      error: false,
+      alertMessage: '',
+
+    };
+  }
+
+  componentDidMount() {
+    axios.get(URL_DATA)
+      .then(response => this.setState({ properties: response.data }))
+      .catch(() => { 
+        this.setState({
+          error: true,
+          alertMessage: 'Unable to load properties. Please try again later.',
+        });
+      });
+  }
+
+  componentDidUpdate(prevProps) {
+    const { search } = this.props.location;
+    if (prevProps.location.search !== search) {
+      axios.get(`http://localhost:3000/api/v1/PropertyListing${search}`)
+        .then(({ data: properties }) => this.setState({ properties }))
+        .catch(err => console.error(err));
+    }
+  }
+
+  buildQueryString = (operation, valueObj) => {
+    const { location: { search } } = this.props;
+
+    const currentQueryParams = qs.parse(search, { ignoreQueryPrefix: true });
+
+    const newQueryParams = {
+      ...currentQueryParams,
+      [operation]: JSON.stringify(valueObj)
+    };
+  };
+
+  render() {
+    return (
+      <div className="properties">
+        <div className="sideBar">
+        <span className="filter"> Filter by City </span>
+          <Link to={'/?query={"city": "Manchester"'}> Manchester </Link> 
+          <Link to={'/?query={"city": "Sheffield"}'}> Sheffield </Link> 
+          <Link to={'/?query={"city": "Leeds"}'}> Leeds </Link> 
+          <Link to={'/?query={"city": "Liverpool"}'}> Liverpool </Link> 
+        </div>
+
+        {this.state.properties.map(property => (
+          <div key={property._id} className="col">
+            <PropertyCard {...property} />
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default Properties;
